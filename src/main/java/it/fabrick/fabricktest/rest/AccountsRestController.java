055e3ac032766fc432d9efb9d79992d8e0673df8
f4803 <andrea.patane'@italiaonline.it>
2024-01-05 16:13:10 +0100

    fourth commit

--- src/main/java/it/fabrick/fabricktest/rest/AccountsRestController.java
+++ src/main/java/it/fabrick/fabricktest/rest/AccountsRestController.java
@@ -3,13 +3,16 @@ package it.fabrick.fabricktest.rest;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JavaType;
 import it.fabrick.fabricktest.client.FabrickRestClient;
+import it.fabrick.fabricktest.client.responsewrapper.FabrickRestClientResponseWrapper;
 import it.fabrick.fabricktest.config.properties.FabrickApiProperties;
 import it.fabrick.fabricktest.rest.model.accountbalance.response.AccountBalance;
 import it.fabrick.fabricktest.rest.model.response.FabrickApiResponse;
+import it.fabrick.fabricktest.rest.model.response.error.FabrickApiError;
 import it.fabrick.fabricktest.utils.JacksonUtils;
 import it.fabrick.fabricktest.utils.ObjectMapperFactory;
 import jakarta.validation.constraints.NotBlank;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.methods.CloseableHttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -50,25 +53,34 @@ public class AccountsRestController {
 
         log.info(">>> [getAccountBalance][RX]: 'accountId':'{}'", accountId);
 
-        String url = apiProperties.getBaseUrl() + apiProperties.getAccountBalancePath();
+        final String url = apiProperties.getBaseUrl() + apiProperties.getAccountBalancePath();
 
         Map<String, String> urlParams = new HashMap<>();
         urlParams.put("accountId", accountId);
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url);
 
-        String uri = builder.buildAndExpand(urlParams).toUriString();
+        final String uri = builder.buildAndExpand(urlParams).toUriString();
 
-        String responseStr = client.get(uri);
+        final FabrickRestClientResponseWrapper responseWrapper = client.get(uri);
+        final HttpStatus status = responseWrapper.getStatus();
 
         JavaType javaType = JacksonUtils.getJavaType(FabrickApiResponse.class, AccountBalance.class);
-        FabrickApiResponse<AccountBalance> jsonResponse = JacksonUtils.deserializeJsonAs(responseStr, javaType);
+        FabrickApiResponse<AccountBalance> jsonResponse = JacksonUtils.deserializeJsonAs(responseWrapper.getResponseBody(), javaType);
+        String responseStr;
 
-        AccountBalance payload = jsonResponse.getPayload();
+        if (status.is2xxSuccessful()) {
+            AccountBalance payload = jsonResponse.getPayload();
+            responseStr = JacksonUtils.writeAsString(payload);
+        }
+        else {
+            FabrickApiError error = jsonResponse.getErrors().get(0);
+            responseStr = JacksonUtils.writeAsString(error);
+        }
 
         log.info("<<< [getAccountBalance][OK]: 'accountId':'{}'", accountId);
 
-        return new ResponseEntity<>(payload, HttpStatus.OK);
+        return new ResponseEntity<>(responseStr, status);
 
     }
 
